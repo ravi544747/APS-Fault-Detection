e4343f095f4c5653e5590625e0ac8c567104f226
Ravi <ravi544747@gmail.com>
2022-12-03 12:11:18 +0530

    code updated data-ingestion

--- data_dump.py
+++ data_dump.py
@@ -1,7 +1,6 @@
 import pymongo
 import pandas as pd
 import json
-
 # Provide the mongodb localhost url to connect python to mongodb.
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
@@ -11,13 +10,14 @@ COLLECTION_NAME="sensor"
 
 
 if __name__=="__main__":
-    df=pd.read_csv(DATA_FILE_PATH)
-    print(f"Rows and Columns:, {df.shape}")
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
 
-    # convert data frame to .Json so that we can dump these records in mongo db
-    df.reset_index(drop=True, inplace=True)
-    json_record=list(json.loads(df.T.to_json()).values())
+    #Convert dataframe to json so that we can dump these record in mongo db
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
-    
-    # insert converted json records to mango db
+
+    #insert converted json record to mongo db
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
